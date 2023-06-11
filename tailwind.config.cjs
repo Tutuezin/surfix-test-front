b86117baefc3e3ffc5d90bdd4420bef7da4cc90e
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-06-11 20:41:52 -0300

    chore: add colors and pictures

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,27 @@ module.exports = {
   mode: "JIT",
   content: ["./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        blue: "#003B7D",
+        lblue: "#006A81",
+        green: "#63AE12",
+        yellow: "#FF9B00",
+      },
+      boxShadow: {
+        bxs: "0 0 30px 0 rgba(0,0,0,.1)",
+      },
+      backgroundImage: {
+        surfixLogoPng: "url(../../assets/imgs/surfixLogo.png)",
+      },
+    },
+    screens: {
+      tablet: { max: "830px" },
+      services: { max: "415px" },
+    },
   },
   plugins: [
     plugin(function ({ addComponents }) {
