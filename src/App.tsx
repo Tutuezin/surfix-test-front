d17d8d104988a01072ccf5f7f561645e8156a58c
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-06-11 15:15:13 -0300

    initial commit!

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "components/Header";
+import "./assets/styles/App.css";
+import Home from "@pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="">
+      <div>Header</div>
+    </header>
+  );
+}
