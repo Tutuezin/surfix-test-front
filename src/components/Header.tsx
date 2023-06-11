9d2d3a04e3d5868059c57a86b070eb33baedac7a
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-06-11 20:41:21 -0300

    feat: create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="">
-      <div>Header</div>
+    <header className="flex ">
+      <div className="bg-surfixLogoPng w-[15rem] h-[5rem] bg-center bg-no-repeat bg-cover mx-auto"></div>
     </header>
   );
 }
